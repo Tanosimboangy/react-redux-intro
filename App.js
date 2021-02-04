c38ac9a950b439d063375628529a0f0ad695849e
Tanosimboangy <jacquit.aja@onja.org>
2021-02-04 15:31:04 +0300

    redu

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 function App(props) {
-	return <div></div>;
+	return <div>
+		<div>{props.count}</div>
+		<button>increment</button>
+		<button>decrement</button>
+	</div>;
 }
 
 export default App;
--- redux/index.js
+++ redux/index.js
@@ -1,17 +1,36 @@
 import redux, { createStore } from 'redux';
 
-/**
- * Challenge: set up redux action creators, reducer, and store
- * We'll be building a counter app to start out.
- * Read the comments below for the step-by-step challenges
- */
+// Create action creators for having the count "increment" and "decrement"
+export function increment() {
+    return {
+        type: "INCREMET",
+    }
+}
+export function decrement() {
+    return {
+        type: "DECREMET",
+    }
+}
 
-// 1. Create action creators for having the count "increment" and "decrement"
+// Create a reducer to handle your increment and decrement actions
+export default function countReducer(count = 0, action) {
+    switch(action.type) {
+        case "INCREMENT" :
+            return count + 1;
+        case "DECREMENT" :
+            return count - 1;
+        default:
+            return count;
+    }
+}
 
-// 2. Create a reducer to handle your increment and decrement actions
+// Create a new Redux store
+const store = createStore(reducer);
 
-// 3. Create a new Redux store
+// Set up the subscribe function so we can more easily see the changes to the Redux state as they happen
+store.subscribe(() => {
+    console.log(store.getState())
+})
 
-// 4. Set up the subscribe function so we can more easily see the changes to the Redux state as they happen
-
-// 5. Export the store as a default export
+// Export the store as a default export
+export default store;
